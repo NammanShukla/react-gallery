d5a3886f95d8517758a1c627a23d6b14ec1140bf
NammanShukla <namman.shukla@gmail.com>
2025-06-04 13:44:38 +0530

    Refactor the code to fit the react framework

--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Header from './components/Header'
+import Carousel from './components/Carousel'
+import Modal from './components/Modal'
+
+const API_KEY = '50323338-d99a3daef9001148f99f6ca32'
+const URL = `https://pixabay.com/api/?key=${API_KEY}&q=landscape&image_type=photo&per_page=15`
 
 function App() {
+  const [images, setImages] = useState([])
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [modalImg, setModalImg] = useState(null)
+
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits.map((img) => img.webformatURL))
+      })
+  }, [])
+
+  const moveSlide = (dir) => {
+    const containerWidth = document.querySelector('.carousel-container')?.clientWidth || 1000
+    const imageWidth = 210
+    const visibleImages = Math.floor(containerWidth / imageWidth)
+    const maxSlide = images.length - visibleImages
+
+    setCurrentSlide((prev) => {
+      const newSlide = Math.min(Math.max(prev + dir, 0), maxSlide)
+      return newSlide
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Carousel
+        images={images}
+        currentSlide={currentSlide}
+        moveSlide={moveSlide}
+        onImageClick={setModalImg}
+      />
+      {modalImg && <Modal src={modalImg} onClose={() => setModalImg(null)} />}
+    </>
+  )
 }
 
-export default App;
+export default App
