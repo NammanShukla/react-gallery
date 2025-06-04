9831fffe2b29be4a4e3df10289b54a232ed64160
NammanShukla <namman.shukla@gmail.com>
2025-06-04 14:01:37 +0530

    Add Carousel and Modal

--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Carousel from './components/Carousel'
-import Modal from './components/Modal'
+import ModalViewer from './components/Modal'
 
 const API_KEY = '50323338-d99a3daef9001148f99f6ca32'
 const URL = `https://pixabay.com/api/?key=${API_KEY}&q=landscape&image_type=photo&per_page=15`
 
 function App() {
   const [images, setImages] = useState([])
-  const [currentSlide, setCurrentSlide] = useState(0)
   const [modalImg, setModalImg] = useState(null)
 
   useEffect(() => {
@@ -19,28 +18,11 @@ function App() {
       })
   }, [])
 
-  const moveSlide = (dir) => {
-    const containerWidth = document.querySelector('.carousel-container')?.clientWidth || 1000
-    const imageWidth = 210
-    const visibleImages = Math.floor(containerWidth / imageWidth)
-    const maxSlide = images.length - visibleImages
-
-    setCurrentSlide((prev) => {
-      const newSlide = Math.min(Math.max(prev + dir, 0), maxSlide)
-      return newSlide
-    })
-  }
-
   return (
     <>
       <Header />
-      <Carousel
-        images={images}
-        currentSlide={currentSlide}
-        moveSlide={moveSlide}
-        onImageClick={setModalImg}
-      />
-      {modalImg && <Modal src={modalImg} onClose={() => setModalImg(null)} />}
+      <Carousel images={images} onImageClick={setModalImg} />
+      <ModalViewer src={modalImg} onClose={() => setModalImg(null)} />
     </>
   )
 }
